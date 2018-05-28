dd2ad24f08c9d2c980413e34c9be5f55fe7b42c0
Tau <tau@tau.zone>
2018-05-28 23:19:37 +0100

    Update boilerplate.

--- tau/manifests/users.pp
+++ tau/manifests/users.pp
@@ -1,15 +1,21 @@
-# $Tau: puppet/modules/tau/manifests/users.pp $
+# @(#) $Tau: puppet/modules/tau/manifests/users.pp $
 
 ## ------------------------------------------------------------------------- ##
 ##                          T A U    P R O J E C T                           ##
 ## ------------------------------------------------------------------------- ##
 ##                                                                           ##
-## This file forms part of the Tau Project and is subject to copyright.      ##
+##  Licensed under the Apache License, Version 2.0 (the "License");          ##
+##  you may not use this file except in compliance with the License.         ##
 ##                                                                           ##
-## For full licensing terms, including conditions for redistribution, please ##
-## consult the README document accompanying the source distribution:         ##
+##  You may obtain a copy of the License at:                                 ##
 ##                                                                           ##
-##   <https://github.com/tauproject/puppet>                                  ##
+##      http://www.apache.org/licenses/LICENSE-2.0                           ##
+##                                                                           ##
+##  Unless required by applicable law or agreed to in writing, software      ##
+##  distributed under the License is distributed on an "AS-IS" BASIS,        ##
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. ##
+##  See the License for the specific language governing permissions and      ##
+##  limitations under the License.                                           ##
 ##                                                                           ##
 ## ------------------------------------------------------------------------- ##
 
